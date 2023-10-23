67b2adccde6bf81faa73b31bbb21cf76a9546e21
Gabriel <tanase.gabriel671@gmail.com>
2023-10-23 12:54:02 +0300

    Finished Play one round function

--- code.js
+++ code.js
@@ -1,9 +1,41 @@
 function getComputerChoice (){
 
 const possibleChoices =['rock','paper','scissors'];
-const computerChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
 
-return computerChoice;
+return randomChoice;
 
 }
-console.log(getComputerChoice());
+
+function playRound() {
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose Rock paper or scissors:").toLowerCase();
+let playerScore =0;
+let computerScore =0;
+let results=[playerScore,computerScore];
+
+if(playerSelection===computerSelection){
+
+    return 'Tie!';
+}
+
+else if((playerSelection==="rock"&&computerSelection==="scissors")||
+(playerSelection==="scissors"&&computerSelection==="paper")||
+(playerSelection==="paper"&&computerSelection==="rock")){
+
+    playerScore++;
+}
+else {
+    computerScore++;
+}
+
+if (playerScore>computerScore) {
+
+    return playerScore;
+}
+else return computerScore;
+
+}
+
+console.log(playRound());
